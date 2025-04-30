5af8aaa410439f6b9fa50fa448626c8fae579d07
unknown <majkromaks1@gmail.com>
2025-04-30 12:05:22 +0300

    Загрузка проекта без докерфайлов для фронта

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+<<<<<<< HEAD
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -52,10 +53,28 @@ class User extends Authenticatable implements MustVerifyEmail
         return $this->createToken($name, $abilities);
     }
     
+=======
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+>>>>>>> f90afea8 (Загрузка проекта без докерфайлов для фронта)
     protected $fillable = [
         'name',
         'email',
         'password',
+<<<<<<< HEAD
         'phone',
         'provider',
         'provider_id',
@@ -63,11 +82,21 @@ class User extends Authenticatable implements MustVerifyEmail
         'is_active',      
     ];
 
+=======
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+>>>>>>> f90afea8 (Загрузка проекта без докерфайлов для фронта)
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
+<<<<<<< HEAD
     protected $casts = [
         'email_verified_at' => 'datetime',
         'phone_verified_at' => 'datetime',
@@ -130,5 +159,18 @@ class User extends Authenticatable implements MustVerifyEmail
     public function completedOrders()
     {
         return $this->hasMany(Order::class)->where('status', Order::STATUS_DELIVERED);
+=======
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+>>>>>>> f90afea8 (Загрузка проекта без докерфайлов для фронта)
     }
 }
