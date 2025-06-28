25dd1871973788d36d54d1765c9a46090c17123f
nikolay_tamindarov <kolyatamindarov@gmail.com>
2025-06-28 18:13:44 +0300

    creating scss elemnts

--- frontend/src/components/Products/ProductItem.js
+++
@@ -1,7 +0,0 @@
-import styles from "./ProductItem.module.css";
-
-const ProductItem = (props) => {
-  return <li className={styles.product}>{props.children}</li>;
-};
-
-export default ProductItem;
--- frontend/src/components/UI/Section.js
+++
@@ -1,7 +0,0 @@
-import styles from "./Section.module.css";
-
-const Section = (props) => {
-  return <section className={styles.section}>{props.children}</section>;
-};
-
-export default Section;
