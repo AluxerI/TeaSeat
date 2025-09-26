adfee28bc6716d9bd4c03956b1b9db3e7e33a998
Skinixxx <81488959+Skinixxx@users.noreply.github.com>
2025-09-26 11:06:21 +0000

    based create http and hook useFetch

--- frontend/src/hooks/useFetch.ts
+++ frontend/src/hooks/useFetch.ts
@@ -1,10 +1,23 @@
 import { useEffect, useState } from "react";
+import { http, HttpStatus } from "../api/http";
 
-function useFetch<T>(utl:string){
+function useFetch<T>(url:string){
     const [data, setData] = useState<T | null>(null);
     const [error, setError] = useState<Error | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
 
-    useEffect
+    useEffect(() => {
+        http.get<T>(url)
+            .then(response => {
+                if(response.status >= HttpStatus.OK && response.status < HttpStatus.MULTIPLE_CHOICES)
+                    setData(response.data);
+                else
+                    setError(new Error(`Error: ${response.status} ${response.statusText}`));
+            }     
+        )
+            .catch(err => setError(err))
+            .finally(()=>setLoading(false));
+    },[url]);
 
+    return {data,error,loading};
 }
