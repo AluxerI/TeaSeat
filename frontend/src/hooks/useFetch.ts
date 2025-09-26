974fa60556f26e6edcafb534a1fc551af767ddba
Skinixxx <81488959+Skinixxx@users.noreply.github.com>
2025-09-26 13:58:51 +0000

    some dev into useFetch

--- frontend/src/hooks/useFetch.ts
+++ frontend/src/hooks/useFetch.ts
@@ -1,10 +1,25 @@
-import { useEffect, useState } from "react";
-import { http, HttpStatus } from "../api/http";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { http, HttpError, HttpStatus } from "../api/http";
 
 function useFetch<T>(url:string){
     const [data, setData] = useState<T | null>(null);
-    const [error, setError] = useState<Error | null>(null);
+    const [error, setError] = useState<HttpError | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
+    const abortControllerRef = useRef<AbortController>(null);
+
+    const execute = useCallback(async(url:string,options:Parameters<typeof  ):{
+        if(abortControllerRef.current){
+            abortControllerRef.current.abort();
+        }
+        abortControllerRef.current = new AbortController();
+        setLoading(true);
+        setError(null);
+
+        try{
+            const response = await http.get<T>(url,{})
+        }
+
+    })
 
     useEffect(() => {
         http.get<T>(url)
