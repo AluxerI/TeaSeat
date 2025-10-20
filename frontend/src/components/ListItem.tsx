32668dca10130462fbd3139db9eaf1aa9a6de8b5
nikolay_tamindarov <kolyatamindarov@gmail.com>
2025-10-20 12:10:09 +0300

    listItem create

--- frontend/src/components/ListItem.tsx
+++ frontend/src/components/ListItem.tsx
@@ -1,7 +1,8 @@
 
 import { PropTypes } from "@material-ui/core"
 import { teaTypes } from "../types"
-import React from "react";
+import React, { JSX } from "react";
+import { catalogItem } from "./catalog";
 
 type MaxWidthType = 
   | 'maxWidthXs'
@@ -10,20 +11,57 @@ type MaxWidthType =
   | 'maxWidthLg'
   | 'maxWidthXl';
 
-interface UniversalListProps<T>{
+type Direction = 'left'|'right'
+
+  interface UniversalListProps<T>{
     items:T[],
     cardWidth:number,
     cardHeight:number,
     gap:number,
+    direction: Direction,
     columns:number,
     rows:number,
     alignment: teaTypes.alignmentType,
-    maxWidth: MaxWidthType
-    
+    maxWidth: MaxWidthType,
+
+    onClick?: (item:T,index:number) => void;
 }
-const UniversalList = ({
-    items,
+const UniversalList = <T,>(
+{
+  items,
+  gap,
+  columns,
+  direction,
+  rows,
+  alignment,
+  maxWidth,
 
-}):React.FC<ListInt=>{
+  onClick
+}:UniversalListProps<T>
+) =>{
 
+
+  const listItems = items.map((item,index) => (
+    <li key={index}
+    >{String(item)}</li>
+  ));
+
+  if(alignment== )
+
+  const styleList = {
+  gap:`${gap}px`,
+  maxWidth: `${maxWidth}px;`,
+  
+
+  }
+
+  return(
+    <div 
+    
+    className="list"
+    style=
+    >
+      {listItems}
+    </div>
+  )
 }
