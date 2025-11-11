48ced4bfd20328f5c06724b6faae40feeddbe0b1
nikolay_tamindarov <kolyatamindarov@gmail.com>
2025-11-11 08:31:58 +0300

    edit grid

---
+++ frontend/src/components/catalogList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { catalogItemProp } from "./catalog";
+
+interface catalogList{
+    catalogs:catalogItemProp[],
+    messendge:string,
+    
+}
+const catalogList:React.FC<>=(catalogs:catalogItemProp[])=>{
+    
+    
+}
