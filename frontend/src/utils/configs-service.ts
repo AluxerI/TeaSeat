ee956dd18f93def1d7018146deca89602ca1f8d0
Skinixxx <81488959+Skinixxx@users.noreply.github.com>
2026-02-05 14:31:57 +0000

    Start clients api

--- frontend/src/utils/configs-service.ts
+++ frontend/src/utils/configs-service.ts
@@ -1,5 +1,5 @@
 import { UseHttpConfig } from "../hooks/useHttp"
-import { Item, User } from "../types/clients.api"
+import { Product, User } from "../types/clients.api"
 
 export const configUser: UseHttpConfig<User> = {
     url: '/user',
@@ -9,7 +9,7 @@ export const configUser: UseHttpConfig<User> = {
     onError:(error)=>{console.log(`Ошибка загрузки пользователя ${error.name} статус: ${error.status},(${error.statusText}). ${error.message}`)}
 }
 
-export const configItem:UseHttpConfig<Item>={
+export const configItem:UseHttpConfig<Product>={
     url:'/item',
     method:'GET',
     autoExecute:true,
