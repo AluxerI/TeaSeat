07c008e2535380931fe05ecfef401a7df752b764
walter <kolyatamindarov@gmail.com>
2026-03-03 11:08:50 +0300

    Complete product List

--- frontend/src/interfaces/catalog.ts
+++ frontend/src/interfaces/catalog.ts
@@ -1,5 +1,6 @@
 import { Warehouse } from "./warehouse";
 
+<<<<<<< HEAD
 export interface Category {
   id: number;
   name: string;
@@ -15,6 +16,47 @@ export interface Category {
 <<<<<<< HEAD
   }[];
 =======
+=======
+export interface Category{
+    id:number;
+    name:string;
+    subcategories:{
+        id:number;
+        name:string;
+        category_id:number;
+        sub_subcategories:{
+            id:number;
+            name:string;
+            subcategory_id:number;
+        }[]
+    }[]
+}
+export interface Product{
+    id:number;
+    name:string;
+    ingredients:string;
+    description:string;
+    image:{
+        background:string;
+        product:string;
+    }
+    pricing:{
+        base_price: string;
+        price_with_promotions:number;
+        final_price:number;
+        promotion_discount:number;
+        personal_discount:number;
+        has_discount:boolean;
+    };
+    weight_grams:number;
+    brand:string;
+    category_path:{
+        category:string;
+        subcategory:string;
+        sub_subcategory:string;
+    };
+    inventory: Warehouse[];
+>>>>>>> a9a77366 (Complete product List)
     total_quantity:number;
     is_available:boolean;
     sold_count:number;
