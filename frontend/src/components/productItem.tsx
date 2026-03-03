33b8852a81c4fef4fdde799d5db38ab95f391c7b
Skinixxx <kolyatamindarov@gmail.com>
2026-03-03 18:10:29 +0300

    Fix some problems with receiving data and start style card

--- frontend/src/components/productItem.tsx
+++ frontend/src/components/productItem.tsx
@@ -4,96 +4,90 @@ import { Discount, Rating } from "../types/catalog";
 import { Picture } from "../types/utils";
 import Images from "../utils/Images";
 
+interface ProductProp {
+  background: Picture;
+  pic_product: Picture;
+  label: string;
+  brand: string;
 
-interface ProductProp{
-    background:Picture;
-    pic_product:Picture;
-    label:string;
-    brand:string;
+  pricing: number;
+  weight: number;
+  //rating: Rating;
+  has_discount: boolean;
+  discounts: Discount[];
 
-    pricing:number;
-    weight:number;
-    //rating: Rating;
-    has_discount: boolean;
-    discounts: Discount[];
-
-    which_category: Category;
-    is_available: boolean;
-    inventory: Warehouse[];
-    date_create: Date;
-    date_update: Date;
-
+  which_category: Category;
+  is_available: boolean;
+  inventory: Warehouse[];
+  date_create: Date;
+  date_update: Date;
 }
 
-export const ProductItem=
-({
-    background,
-    pic_product,
-    label,
-    brand,
-    pricing,
-    weight,
-    //rating,
-    has_discount,
-    discounts,
-    which_category,
-    is_available,
-    inventory,
-    date_create,
-    date_update,
+export const ProductItem = ({
+  background,
+  pic_product,
+  label,
+  brand,
+  pricing,
+  weight,
+  //rating,
+  has_discount,
+  discounts,
+  which_category,
+  is_available,
+  inventory,
+  date_create,
+  date_update,
+}: ProductProp) => {
+  let step = 5;
 
-}:ProductProp)=>{
-    let step = 5
-
-    const upWeight=()=>{
-    weight+=step
-    }
-    const downWeight=()=>{
-    weight-=step
-    }
-    return <div className="product-card">
-        <div className="head-card">
-            <Images
-            name={pic_product.name}
-            alt={pic_product.alt}
-            type={pic_product.type}
-            className="background-card"
-            >
-            </Images>
-        </div>
-        <div className="body-card">
-            <section className="section">
-                <h5 className="label-product">{label}</h5>
-                <div className="gramm-and-price">
-                    <form action="" className="grams-form">
-                        <Images className="background" 
-                            name={background.name}
-                            alt={background.alt}
-                            type={background.type}
-                            
-                        ></Images>
-                        <p className="count-gramms">{weight}</p>
-                        <button type="button" onClick={upWeight}>
-                            <Images
-                            name="https://cdn-icons-png.flaticon.com/512/271/271239"
-                            type="png"
-                            alt="icon-up"
-                            >
-                            </Images>
-                        </button>
-                        <button type="button" onClick={downWeight}>
-                            
-                            <Images
-                            name="https://cdn-icons-png.flaticon.com/512/271/271210"
-                            type="png"
-                            alt="icon-down">
-                            </Images>
-                        </button>
-                    </form>
-                    <p className="price">{pricing}</p>
-                </div>
-
-            </section>
-        </div>
+  const upWeight = () => {
+    weight += step;
+  };
+  const downWeight = () => {
+    weight -= step;
+  };
+  return (
+    <div className="product-card">
+      <div className="head-card">
+        <Images
+          name={pic_product.name}
+          alt={pic_product.alt}
+          type={pic_product.type}
+          className="background-card"
+        ></Images>
+      </div>
+      <div className="body-card">
+        <section className="section">
+          <h5 className="label-product">{label}</h5>
+          <div className="gramm-and-price">
+            <form action="" className="grams-form">
+              <Images
+                className="background"
+                name={background.name}
+                alt={background.alt}
+                type={background.type}
+              ></Images>
+              <p className="count-gramms">{weight}</p>
+              <button type="button" onClick={upWeight}>
+                <Images
+                  name="https://cdn-icons-png.flaticon.com/512/271/271239"
+                  type="png"
+                  alt="icon-up"
+                ></Images>
+              </button>
+              <button type="button" onClick={downWeight}>
+                <Images
+                  name="https://cdn-icons-png.flaticon.com/512/271/271210"
+                  type="png"
+                  alt="icon-down"
+                ></Images>
+              </button>
+            </form>
+            <p className="price">{pricing}</p>
+          </div>
+        </section>
+      </div>
     </div>
-}
+  );
+};
--- frontend/src/components/productList.tsx
+++ frontend/src/components/productList.tsx
@@ -7,55 +7,64 @@ import { Picture } from "../types/utils";
 import { typePic } from "./catalog";
 import { Discount } from "../types/catalog";
 import { CategoryAPI } from "../api/categoryAPI";
+import { useMemo } from "react";
 
+export const ProductList = () => {
+  const products = useAsync(() => catalogApi.getProducts(), true);
+  const category = useAsync(() => catalogApi.getCategory(), true);
 
+  const EnrichedProducts = useMemo(() => {
+    if (!products || !category) return [];
+    const categoryMap = new Map(category.data?.map((c) => [c.name, c]));
+    return products.data?.map((product) => ({
+      ...product,
+      category_path: {
+        category: categoryMap.get(product.category_path.category),
+      },
+    }));
+  }, [products, category]);
 
-export const ProductList = ()=>{
-    const products = useAsync(()=> catalogApi.getProducts(),true)
-    const catAPI = CategoryAPI
-    
-    if(products.data !== null)
-        if(products.data.length)
-        for(const arr of products.data!){
-            console.log(arr)
-            
-        }
-
-    return (
+  return (
     <Grid
-    container
-    alignItems={"center"}
-    display={"flex"}
-
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      className="grid-category"
     >
-        <div className="item">
-            {
-            
-            products.data?.map((value:Product)=>{
+      {EnrichedProducts?.map((value) => {
+        console.log(value.images.background.split("."));
+        const part_background: Picture = {
+          name: value.images.background.split(".")[0],
+          type: value.images.background.split(".")[1] as typePic,
+          alt: value.images.background.split(".")[0],
+        };
+        const part_product: Picture = {
+          name: value.images.main.split(".")[0],
+          type: value.images.main.split(".")[1] as typePic,
+          alt: value.images.main.split(".")[0],
+        };
 
-                const part_background:Picture = {name:value.image.background.split('.')[0],type:value.image.background.split('.')[1] as typePic, alt:value.image.
-                    background.split('.')[0]}
-                const part_product:Picture = {name:value.image.product.split('.')[0],type:value.image.product.split('.')[1] as typePic, alt:value.image.
-                    product.split('.')[0]}
-                const category = await catAPI.getCategoryByName(value.category_path.category) 
-
-                return(
-                <ProductItem background={part_background} brand={value.brand}
-                label={value.name} weight={value.weight_grams} has_discount={value.pricing.has_discount}
-                discounts={Array(Discount.form(value.pricing.personal_discount))}
-                pic_product={part_product}
-                inventory={value.inventory}
-                is_available={value.is_available && value.total_quantity>0}
-                pricing={value.sold_count}
-                key={value.id}
-                which_category={category!} // not safefull
-                //rating={}
-                date_create={value.created_at}
-                date_update={value.update_at}
-                ></ProductItem>
-            )})}
-        </div>
-        
+        return (
+          <ProductItem
+            background={part_background}
+            brand={value.brand}
+            label={value.name}
+            weight={value.weight_grams}
+            has_discount={value.pricing.has_discount}
+            discounts={Array(Discount.form(value.pricing.personal_discount))}
+            pic_product={part_product}
+            inventory={value.inventory}
+            is_available={value.is_available && value.total_quantity > 0}
+            pricing={value.sold_count}
+            key={value.id}
+            which_category={value.category_path.category!} // not safefull
+            //rating={}
+            date_create={value.created_at}
+            date_update={value.update_at}
+          ></ProductItem>
+        );
+      })}
     </Grid>
-    )
-}
+  );
+};
