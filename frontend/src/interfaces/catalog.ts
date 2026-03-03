656759bdac38bf431b42541f7f972389f6109c5e
Skinixxx <kolyatamindarov@gmail.com>
2026-03-03 18:10:29 +0300

    Fix some problems with receiving data and start style card

--- frontend/src/interfaces/catalog.ts
+++ frontend/src/interfaces/catalog.ts
@@ -19,9 +19,9 @@ export interface Product{
     name:string;
     ingredients:string;
     description:string;
-    image:{
+    images:{
         background:string;
-        product:string;
+        main:string;
     }
     pricing:{
         base_price: string;
