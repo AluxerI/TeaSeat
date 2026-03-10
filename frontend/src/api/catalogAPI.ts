b85aa3ce44c10135cb1eddac7829fe69362bc5a2
Skinixxx <kolyatamindarov@gmail.com>
2026-03-10 17:56:56 +0300

    Past complete part card product

--- frontend/src/api/catalogAPI.ts
+++ frontend/src/api/catalogAPI.ts
@@ -1,44 +1,41 @@
-
 import { Catalog, Category, Meta, Product } from "../interfaces/catalog";
 import { Json } from "../types/utils";
-import { api } from "./api"
+import { api } from "./api";
 
+const catalog_path = `/catalog`;
 
-const catalog_path = `/catalog`
-
-async function getCatalog():Promise<Catalog|undefined>{
-    try{
-        const response = await api.get<Catalog>(catalog_path);
-        return response.data as Catalog}
-    catch(e:unknown){
-        if(e instanceof Error){
-            console.error(`Error when loading catalog: ${e}`)
-        }
-        else{
-            console.error(`Unknow error`);
-        }
-        return undefined
+async function getCatalog(): Promise<Catalog | undefined> {
+  try {
+    const response = await api.get<Catalog>(catalog_path);
+    return response.data as Catalog;
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      console.error(`Error when loading catalog: ${e}`);
+    } else {
+      console.error(`Unknow error`);
     }
-    }
-async function parseCatalog(){
-    let catalog:Catalog|string = (await getCatalog())!;
-    catalog=JSON.stringify(catalog);
-    const answer:Json = JSON.parse(catalog)
-    const ans:Catalog = answer['data'] as Catalog
-    return ans
+    return undefined;
+  }
+}
+async function parseCatalog() {
+  let catalog: Catalog | string = (await getCatalog())!;
+  catalog = JSON.stringify(catalog);
+  const answer: Json = JSON.parse(catalog);
+  const ans: Catalog = answer["data"] as Catalog;
+  return ans;
 }
 
 export const catalogApi = {
-    async getProducts():Promise<Product[]>{
-        const ans:Catalog = await parseCatalog()
-        return ans['products'] as Product[]
-    },
-    async getCategory():Promise<Category[]>{
-        const ans:Catalog = await parseCatalog()
-        return ans['categories'] as Category[] 
-    },
-    async getMeta():Promise<Meta>{
-        const ans:Catalog = await parseCatalog()
-        return ans['meta'] as Meta
-    }
-}
+  async getProducts(): Promise<Product[]> {
+    const ans: Catalog = await parseCatalog();
+    return ans["products"] as Product[];
+  },
+  async getCategory(): Promise<Category[]> {
+    const ans: Catalog = await parseCatalog();
+    return ans["categories"] as Category[];
+  },
+  async getMeta(): Promise<Meta> {
+    const ans: Catalog = await parseCatalog();
+    return ans["meta"] as Meta;
+  },
+};
--- frontend/src/interfaces/catalog.ts
+++ frontend/src/interfaces/catalog.ts
@@ -1,56 +1,56 @@
 import { Warehouse } from "./warehouse";
 
-export interface Category{
-    id:number;
-    name:string;
-    subcategories:{
-        id:number;
-        name:string;
-        category_id:number;
-        sub_subcategories:{
-            id:number;
-            name:string;
-            subcategory_id:number;
-        }[]
-    }[]
+export interface Category {
+  id: number;
+  name: string;
+  subcategories: {
+    id: number;
+    name: string;
+    category_id: number;
+    sub_subcategories: {
+      id: number;
+      name: string;
+      subcategory_id: number;
+    }[];
+  }[];
 }
-export interface Product{
-    id:number;
-    name:string;
-    ingredients:string;
-    description:string;
-    images:{
-        background:string;
-        main:string;
-    }
-    pricing:{
-        base_price: string;
-        price_with_promotions:number;
-        final_price:number;
-        promotion_discount:number;
-        personal_discount:number;
-        has_discount:boolean;
-    };
-    weight_grams:number;
-    brand:string;
-    category_path:{
-        category:string;
-        subcategory:string;
-        sub_subcategory:string;
-    };
-    inventory: Warehouse[];
-    total_quantity:number;
-    is_available:boolean;
-    sold_count:number;
-    created_at: Date;
-    update_at: Date;
+export interface Product {
+  id: number;
+  name: string;
+  ingredients: string;
+  description: string;
+  images: {
+    background: string;
+    main: string;
+  };
+  pricing: {
+    base_price: string;
+    price_with_promotions: number;
+    final_price: number;
+    promotion_discount: number;
+    personal_discount: number;
+    has_discount: boolean;
+  };
+  weight_grams: number;
+  brand: string;
+  category_path: {
+    category: string;
+    subcategory: string;
+    sub_subcategory: string;
+  };
+  inventory: Warehouse[];
+  total_quantity: number;
+  is_available: boolean;
+  sold_count: number;
+  created_at: Date;
+  update_at: Date;
 }
-export interface Meta{
-    total_products:number;
-    has_pagination:boolean;
+export interface Meta {
+  total_products: number;
+  has_pagination: boolean;
 }
-export interface Catalog{
-    categories:Category[];
-    products:Product[];
-    meta:Meta;
+export interface Catalog {
+  categories: Category[];
+  products: Product[];
+  meta: Meta;
 }
