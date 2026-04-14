56de0933c01e8d16d26f61c667dfaf59cc372b83
Skinixxx <kolyatamindarov@gmail.com>
2026-04-14 12:33:07 +0300

    Complete update front

--- frontend/src/interfaces/catalog.ts
+++ frontend/src/interfaces/catalog.ts
@@ -1,6 +1,5 @@
 import { Warehouse } from "./warehouse";
 
-<<<<<<< HEAD
 export interface Category {
   id: number;
   name: string;
@@ -13,56 +12,39 @@ export interface Category {
       name: string;
       subcategory_id: number;
     }[];
-<<<<<<< HEAD
   }[];
-=======
-=======
-export interface Category{
-    id:number;
-    name:string;
-    subcategories:{
-        id:number;
-        name:string;
-        category_id:number;
-        sub_subcategories:{
-            id:number;
-            name:string;
-            subcategory_id:number;
-        }[]
-    }[]
 }
-export interface Product{
-    id:number;
-    name:string;
-    ingredients:string;
-    description:string;
-    images:{
-        background:string;
-        main:string;
-    }
-    pricing:{
-        base_price: string;
-        price_with_promotions:number;
-        final_price:number;
-        promotion_discount:number;
-        personal_discount:number;
-        has_discount:boolean;
-    };
-    weight_grams:number;
-    brand:string;
-    category_path:{
-        category:string;
-        subcategory:string;
-        sub_subcategory:string;
-    };
-    inventory: Warehouse[];
->>>>>>> a9a77366 (Complete product List)
-    total_quantity:number;
-    is_available:boolean;
-    sold_count:number;
-    created_at: Date;
-    update_at: Date;
->>>>>>> f092fd80 (Create component - category)
+export interface Product {
+  id: number;
+  name: string;
+  ingredients: string;
+  description: string;
+  images: {
+    background: string;
+    main: string;
+  };
+  pricing: {
+    base_price: string;
+    price_with_promotions: number;
+    final_price: number;
+    promotion_discount: number;
+    personal_discount: number;
+    has_discount: boolean;
+  };
+  weight_grams: number;
+  brand: string;
+  category_path: {
+    category: string;
+    subcategory: string;
+    sub_subcategory: string;
+  };
+  inventory: Warehouse[];
+
+  total_quantity: number;
+  is_available: boolean;
+  sold_count: number;
+  created_at: Date;
+  update_at: Date;
 }
 export interface Product {
   id: number;
