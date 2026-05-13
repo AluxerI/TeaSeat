7d3278ffa76355c28d20acb2bb555a32e1a98f1d
walter <kolyatamindarov@gmail.com>
2026-05-13 23:54:55 +0300

    Create utils file for parsing

--- frontend/src/utils/product_methods.ts
+++ frontend/src/utils/product_methods.ts
@@ -1,17 +1,28 @@
 import { Category, Product } from "../interfaces/catalog";
 
-export const sort_by_category = (category: Category[],products: Product[])=>{
+export const sort_by_category = (category: Category[],products: Product[],what_return : "object" | "count")=>{
 
+    if(what_return === "object"){
+            
     const productByCategory = new Map<string,string[]>()
 
     for(var value of category){
         productByCategory.set(value.name,[])
     }
-
     products.forEach(product => {
         // be bebug
         productByCategory.get(product.category_path.category)?.push(product.id.toString())
 
     });    
+    return productByCategory
+}
+    else{
+        products.forEach(product => {
+        // be bebug
+        productByCategory.get(product.category_path.category)?.push(product.id.toString())
 
+    });
+    return productByCategory
+    }
+    
 }
